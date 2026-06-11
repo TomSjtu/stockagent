f803f14d88ed93d1fa2148be3d51c074111e30b1
tomlu <tom_lu24@163.com>
2026-06-11 17:16:08 +0800

    feat: 添加财务健康度相关代码

--- src/stockagent/fundamentals/__init__.py
+++ src/stockagent/fundamentals/__init__.py
@@ -1,8 +1,14 @@
 from stockagent.fundamentals.cash_flow import compute_cash_flow, compute_cash_flow_series
+from stockagent.fundamentals.financial_health import (
+    compute_financial_health,
+    compute_financial_health_series,
+)
 from stockagent.fundamentals.inputs import (
     CashFlowInput,
+    FinancialHealthInput,
     ProfitabilityInput,
     build_cash_flow_inputs,
+    build_financial_health_inputs,
     build_profitability_inputs,
 )
 from stockagent.fundamentals.profitability import (
@@ -13,10 +19,14 @@ from stockagent.fundamentals.profitability import (
 __all__ = [
     "ProfitabilityInput",
     "CashFlowInput",
+    "FinancialHealthInput",
     "build_cash_flow_inputs",
+    "build_financial_health_inputs",
     "build_profitability_inputs",
     "compute_cash_flow",
     "compute_cash_flow_series",
+    "compute_financial_health",
+    "compute_financial_health_series",
     "compute_profitability",
     "compute_profitability_series",
 ]
